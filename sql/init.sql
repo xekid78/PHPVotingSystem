05700c0cd64f88a680594d3b6e1b642b48d37711
Makoto Tanabe <xekid78@outlook.com>
2018-05-22 10:42:20 +0900

    Change SQL statement additionally

--- sql/init.sql
+++ sql/init.sql
@@ -4,8 +4,13 @@ grant all on poll_php.* to dbuser@localhost identified by '########';
 
 use poll_php
 
+drop table if exists answers;
 create table answers (
   id int not null auto_increment primary key,
-  answers int not null,
-  created datetime
+  answer int not null,
+  created datetime,
+  remote_addr varchar(15),
+  user_agent varchar(255),
+  answer_date date,
+  unique unique_answer(remote_addr, user_agent, answer_date)
 );
